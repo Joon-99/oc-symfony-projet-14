886c1d4ab4d7eec50038b98343b09afc301fb536
Arthur Jolivet <arthur@cie-th.com>
2026-09-23 11:25:53 +0200

    test-ci - revert failure tests

--- config/helpers.php
+++ config/helpers.php
@@ -20,9 +20,3 @@ function array_fill_callback(int $startIndex, int $count, callable $callback): a
 
     return $data;
 }
-
-// phpstan level 6 test: missing parameter and return typehints
-function shouldFailPHPStanLevel6($value)
-{
-    return $value;
-}
